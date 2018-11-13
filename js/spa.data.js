66459f5d8b0cb7ecd54d766df0e00c9742104a87
ARRAY-LIST <1348909544@qq.com>
2018-11-13 20:09:07 +0800

    6.6 avatar

--- js/spa.data.js
+++ js/spa.data.js
@@ -9,7 +9,39 @@
         white  : true
       */
 
-/* global &,spa */
+/* global $, io, spa */
 spa.data = function () {
-  return {};
+  'use strict';
+  var
+    stateMap = {sio: null},
+    makeSio, getSio, initModule;
+  makeSio = function () {
+    // 使用/chat名字空间，创建socket连接
+    var socket = io.connect('/chat');
+    return {
+      // 确保emit方法向服务器发送与给定事件名相关的数据
+      emit: function (event_name, data) {
+        socket.emit(event_name, data);
+      },
+      // 确保on方法注册了给定事件名的回调函数。
+      // 从服务器接收到的任何事件数据会传回给回调函数
+      on: function (event_name, callback) {
+        socket.on(event_name, function () {
+          callback(arguments);
+        })
+      }
+    }
+  };
+  getSio = function () {
+    if (!stateMap.sio) {
+      stateMap.sio = makeSio();
+    }
+    return stateMap.sio;
+  };
+  initModule = function () {
+  };
+  return {
+    getSio: getSio,
+    initModule: initModule
+  };
 }();
--- js/spa.js
+++ js/spa.js
@@ -12,6 +12,7 @@ Root namespace module
 var spa = (function () {
   'use strict';
   var initModule = function ($container) {
+    spa.data.initModule();
     spa.model.initModule();
     spa.shell.initModule($container);
   };
